837dac53108f46cccb11fb3a471c16f58376b28c
Rakibul <mrakibul70@gmail.com>
2020-01-13 16:14:50 +0600

    Ajax using fatch API

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,14 +4,16 @@ const url = 'https://jsonplaceholder.typicode.com/users';
 let btn = document.querySelector('#loadData');
 let p = document.querySelector('#output');
 
+// Fetch API Details
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
 btn.addEventListener('click', function () {
-    const xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = () => {
-        p.innerHTML = xhr.response
-    };
-
-    xhr.open('GET', url);
-    xhr.send()
-
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                data.forEach((user) => {
+                    p.innerHTML = `${p.innerHTML} <br> Name: ${user.name}`
+                })
+            })
+            .catch(err => console.log(err))
 });
